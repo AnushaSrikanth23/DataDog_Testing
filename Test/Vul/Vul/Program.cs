21f7feb0493c4281080e1bd40f0f456d9eb6be85
AnushaSrikanth23 <anushasrikanth95@gmail.com>
2024-10-28 07:18:17 +0000

    Test Code

---
+++ Test/Vul/Vul/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vul
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+ 
+
+            Console.WriteLine("Enter your Order ID:");
+            string orderid = Console.ReadLine();
+
+            // Vulnerable code: SQL Injection risk
+            string query = $"SELECT * FROM Eshop WHERE order_id = '{orderid}'";
+
+            using (SqlConnection connection = new SqlConnection("Server=.\\NEUROIT;User Id=sa;Password=Welcome@123;Initial Catalog=NeuroIT;TrustServerCertificate=True;"))
+            {
+                SqlCommand command = new SqlCommand(query, connection);
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+
+                if (reader.HasRows)
+                {
+                    Console.WriteLine("Login successful!");
+                }
+                else
+                {
+                    Console.WriteLine("Invalid username or password.");
+                }
+            }
+        }
+    }
+}
