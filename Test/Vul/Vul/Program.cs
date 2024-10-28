f0da502fc6d9efb4e9ef28f4e106c4a521bc8b91
AnushaSrikanth23 <anushasrikanth95@gmail.com>
2024-10-28 10:22:03 +0000

    commit check

--- Test/Vul/Vul/Program.cs
+++ Test/Vul/Vul/Program.cs
@@ -16,7 +16,7 @@ namespace Vul
             Console.WriteLine("Enter your Order ID:");
             string orderid = Console.ReadLine();
 
-            // Vulnerable code: SQL Injection risk
+            //// Vulnerable code: SQL Injection risk
             string query = $"SELECT * FROM Eshop WHERE order_id = '{orderid}'";
 
             using (SqlConnection connection = new SqlConnection("Server=.\\NEUROIT;User Id=sa;Password=Welcome@123;Initial Catalog=NeuroIT;TrustServerCertificate=True;"))
